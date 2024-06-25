4f864613e3fb24415739879437ffbdcf96edc07a
jerome papazian <j_papazian2000@yahoo.fr>
2024-06-25 17:17:31 +0200

    add postgres db secret engine, role and user

--- auth.tf
+++ auth.tf
@@ -20,3 +20,26 @@ resource "vault_generic_endpoint" "kv_read_user" {
 }
 EOT
 }
+
+resource "vault_policy" "dw_dw_db_policy" {
+   name       = "dw_postgres_ro_policy"
+   policy     = <<EOF
+path "${vault_database_secrets_mount.dw_db.path}/creds/readonly" {
+   capabilities = ["read"]
+}
+EOF
+}
+
+resource "vault_generic_endpoint" "postgres_read_user" {
+  #depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/pg_user"
+  ignore_absent_fields = true
+  #write_fields         = ["id"]
+
+  data_json = <<EOT
+{
+  "policies": ["dw_postgres_ro_policy"],
+  "password": "changeme"
+}
+EOT
+}
