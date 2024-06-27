05b76ea0e620cb2b403b1aa6c0ad11d7b592787a
jerome papazian <j_papazian2000@yahoo.fr>
2024-06-27 18:10:29 +0200

    add pki

---
+++ secrets-pki.tf
@@ -0,0 +1,16 @@
+resource "vault_mount" "pki" {
+  path                      = "dw_pki"
+  type                      = "pki"
+  default_lease_ttl_seconds = 3600
+  max_lease_ttl_seconds     = 86400
+}
+
+resource "vault_pki_secret_backend_config_urls" "dw_sanofi_backend_urls" {
+  backend = vault_mount.pki.path
+  issuing_certificates = [
+    "${data.tfe_outputs.vault_infra.values.vault_public_url}/v1/pki/ca",
+  ]
+  crl_distribution_points = [ 
+    "${data.tfe_outputs.vault_infra.values.vault_public_url}/v1/pki/crl"
+   ]
+}
