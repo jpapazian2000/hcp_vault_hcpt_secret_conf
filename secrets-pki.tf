91ef71d31700686a855452e6bd628df86f0dee2c
jerome papazian <j_papazian2000@yahoo.fr>
2024-06-28 16:35:45 +0200

    add intermediate CA for pki

--- secrets-pki.tf
+++ secrets-pki.tf
@@ -18,7 +18,7 @@ resource "vault_pki_secret_backend_config_urls" "dw_sanofi_backend_urls" {
 resource "vault_pki_secret_backend_root_cert" "dw_root" {
   backend = vault_mount.pki.path
   type = "internal"
-  common_name = "dw.sanofi.com"
+  common_name = "chc.sanofi.com"
   ttl = "315360000"
   issuer_name = "dw-root-cert"
 }
@@ -36,6 +36,50 @@ resource "vault_pki_secret_backend_role" "dw_role" {
   allow_ip_sans    = true
   key_type         = "rsa"
   key_bits         = 4096
-  allowed_domains  = ["dw.sanofi.com"]
+  allowed_domains  = ["chc.sanofi.com"]
   allow_subdomains = true
+}
+#create an intermediate authority
+resource "vault_mount" "dw_pki_int" {
+   path        = "dw_pki_int"
+   type        = "pki"
+   description = "This is an example intermediate PKI mount"
+
+   default_lease_ttl_seconds = 86400
+   max_lease_ttl_seconds     = 157680000
+}
+resource "vault_pki_secret_backend_intermediate_cert_request" "dw_csr-request" {
+   backend     = vault_mount.dw_pki_int.path
+   type        = "internal"
+   common_name = "chc.sanofi.com Intermediate Authority"
+}
+
+resource "vault_pki_secret_backend_root_sign_intermediate" "dw_intermediate" {
+   backend     = vault_mount.pki.path
+   common_name = "new_intermediate"
+   csr         = vault_pki_secret_backend_intermediate_cert_request.dw_csr-request.csr
+   format      = "pem_bundle"
+   ttl         = 15480000
+   issuer_ref  = vault_pki_secret_backend_root_cert.dw_root.issuer_id
+}
+resource "vault_pki_secret_backend_intermediate_set_signed" "dw_intermediate" {
+   backend     = vault_mount.dw_pki_int.path
+   certificate = vault_pki_secret_backend_root_sign_intermediate.dw_intermediate.certificate
+}
+resource "vault_pki_secret_backend_issuer" "dw_intermediate" {
+  backend     = vault_mount.dw_pki_int.path
+  issuer_ref  = vault_pki_secret_backend_intermediate_set_signed.dw_intermediate.imported_issuers[0]
+  issuer_name = "dw-dot-com-intermediate"
+}
+resource "vault_pki_secret_backend_role" "dw_intermediate_role" {
+   backend          = vault_mount.dw_pki_int.path
+   issuer_ref       = vault_pki_secret_backend_issuer.dw_intermediate.issuer_ref
+   name             = "dw_intermediate-role"
+   ttl              = 86400
+   max_ttl          = 2592000
+   allow_ip_sans    = true
+   key_type         = "rsa"
+   key_bits         = 4096
+   allowed_domains  = ["chc.sanofi.com"]
+   allow_subdomains = true
 }
